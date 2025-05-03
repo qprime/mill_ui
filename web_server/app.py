d260ba7aab95d07874134af30b199c478d608239
qprime <squinlan.programming@gmail.com>
2025-05-03 09:33:51 -0400

    Updates to the jsonl memory reviewer.

--- web_server/app.py
+++ web_server/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route("/")
 def home():
-    return "<h1>Welcome to the Cliff AI Control Panel</h1><p><a href='/lab-manager'>Manage Devices</a></p><p><a href='/voice'>Voice Terminal</a></p>"
+    return "<h1>Welcome to the Cliff AI Control Panel</h1><p><a href='/lab-manager'>Manage Devices</a></p><p><a href='/voice'>Voice Terminal</a></p><p><a href='/jsonl-review'>jsonl review</a></p>"
 
 @app.route("/lab-manager")
 def lab_manager():
@@ -14,9 +14,9 @@ def lab_manager():
 def voice_terminal():
     return render_template("voice_terminal.html")
     
-@app.route("/json-review")
-def voice_terminal():
-    return render_template("json_review.html")
+@app.route("/jsonl-review")
+def jsonl_review():
+    return render_template("jasonl_review.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, ssl_context=("cert/web_server.crt", "cert/web_server.key"), debug=True)

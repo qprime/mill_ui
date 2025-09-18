cb36c0e476083252ba76d67f6229d1c288d52583
qprime <squinlan.programming#gmail.com>
2025-09-18 09:43:03 -0400

    Remove legacy cam_generator

--- skills/cam_generator/gcode_writer.py
+++
@@ -1,8 +0,0 @@
-# path: skills/cam_generator/core/gcode_writer.py
-# # desc: Write G-code lines to disk.
-# api: write_gcode
-# tags: cam
-
-def write_gcode(gcode_lines, out_path):
-    with open(out_path, "w") as f:
-        f.write("\n".join(gcode_lines))
